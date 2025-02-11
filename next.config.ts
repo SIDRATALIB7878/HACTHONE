5911a15b958d353fd21ec4330268d08266d7aaca
SIDRATALIB7878 <164734030+SIDRATALIB7878@users.noreply.github.com>
2025-02-11 21:25:52 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  image :{
+    domains: [`cdn.sanity.io`]  // Add your domain names here
+  }
+  /* config options here */
+};
+
+export default nextConfig;
